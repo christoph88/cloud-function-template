7306497afc1fee5427d7ccaa264b78cfb98baa35
Christoph Geypen <geypenchristoph@gmail.com>
2022-02-23 15:37:31 +0100

    set different routes in function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('env-yaml').config({ path: '.env.yaml' });
+const express = require('express');
 
 /**
  * HTTP Cloud Function.
@@ -8,12 +9,31 @@ require('env-yaml').config({ path: '.env.yaml' });
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
-exports.run = async (req, res) => {
+
+const getId = async (req, res) => {
+  const { id } = req.params;
+  res.status(200).send(`dit is test2 met id ${id}`);
+};
+
+const getTest = async (req, res) => {
+  res.status(200).send('dit is test2');
+};
+
+const getDefault = async (req, res) => {
   const { param } = req.query;
   // eslint-disable-next-line camelcase
   const { test } = process.env;
   res.status(200).send(test);
 };
+
+// Create an Express object and routes (in order)
+const app = express();
+app.use('/test2/:id', getId);
+app.use('/test2/', getTest);
+app.use(getDefault);
+
+// Set our GCF handler to our Express app.
+exports.users = app;
 // Needed for Quokka
 // exports.run(
 //   { query: { q: 'drunk with a gun nosferatu' } },
